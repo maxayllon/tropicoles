5d24b540762ce158da89e709f1f159f98d69b5a1
maxime ayllon <maximeayllon12@gmail.com>
2021-06-22 12:53:58 +0200

    model done

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_22_104701) do
+ActiveRecord::Schema.define(version: 2021_06_22_105102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "playerones", force: :cascade do |t|
+    t.string "name"
+    t.integer "point", default: 0
+    t.bigint "teamone_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["teamone_id"], name: "index_playerones_on_teamone_id"
+  end
+
+  create_table "playertwos", force: :cascade do |t|
+    t.string "name"
+    t.integer "point", default: 0
+    t.bigint "teamtwo_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["teamtwo_id"], name: "index_playertwos_on_teamtwo_id"
+  end
+
+  create_table "ppts", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_ppts_on_user_id"
+  end
+
+  create_table "teamones", force: :cascade do |t|
+    t.string "name"
+    t.integer "point", default: 0
+    t.bigint "ppt_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ppt_id"], name: "index_teamones_on_ppt_id"
+  end
+
+  create_table "teamtwos", force: :cascade do |t|
+    t.string "name"
+    t.integer "point", default: 0
+    t.bigint "ppt_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["ppt_id"], name: "index_teamtwos_on_ppt_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +71,9 @@ ActiveRecord::Schema.define(version: 2021_06_22_104701) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "playerones", "teamones"
+  add_foreign_key "playertwos", "teamtwos"
+  add_foreign_key "ppts", "users"
+  add_foreign_key "teamones", "ppts"
+  add_foreign_key "teamtwos", "ppts"
 end
